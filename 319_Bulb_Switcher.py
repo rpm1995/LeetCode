efa8e19c2849ab513456367c69d010b9eb744a39
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-06-22 11:03:33 -0400

    Brute force approach

---
+++ 319_Bulb_Switcher.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+
+        if n == 1:
+            return 1
+
+        bulbs = [False for _ in range(n)]
+
+        for i in range(1, n + 1):
+            for bulb in range(i, n + 1, i):
+                bulbs[bulb - 1] = bulbs[bulb - 1] ^ True
+
+        ans = 0
+
+        for bulb in bulbs:
+            if bulb is True:
+                ans += 1
+
+        return ans
