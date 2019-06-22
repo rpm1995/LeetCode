7b7bb93d0378d0864c2c39d17f64ec223a13d562
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-06-22 11:17:16 -0400

    Accepted solution

--- 319_Bulb_Switcher.py
+++ 319_Bulb_Switcher.py
@@ -1,19 +1,21 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
+        #         if n == 1:
+        #             return 1
 
-        if n == 1:
-            return 1
+        #         bulbs = [False for _ in range(n)]
 
-        bulbs = [False for _ in range(n)]
+        #         for i in range(1, n+1):
+        #             for bulb in range(i, n+1, i):
 
-        for i in range(1, n + 1):
-            for bulb in range(i, n + 1, i):
-                bulbs[bulb - 1] = bulbs[bulb - 1] ^ True
+        #                 bulbs[bulb-1] = bulbs[bulb-1] ^ True
 
-        ans = 0
+        #         ans = 0
 
-        for bulb in bulbs:
-            if bulb is True:
-                ans += 1
+        #         for bulb in bulbs:
+        #             if bulb is True:
+        #                 ans += 1
 
-        return ans
+        #         return ans
+
+        return int(n ** 0.5)
