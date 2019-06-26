da3e762fd30a4a7e9248155727dfaa63741d97d7
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-06-26 11:44:58 -0400

    236 Accepted Ugly Solution

---
+++ 236_Lowest_Common_Ancestor_Of_A_Binary_Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        def helper(node):
+
+            if not node:
+                return (False, "idgaf")
+
+            L, foundL = helper(node.left)
+            R, foundR = helper(node.right)
+
+            if L and R:
+                return (True, node)
+
+            if node != p and node != q:
+                if L:
+                    return (True, foundL)
+
+                if R:
+                    return (True, foundR)
+
+            if node == p or node == q:
+                return (True, node)
+
+            return (False, "idgaf")
+
+        return helper(root)[1]
