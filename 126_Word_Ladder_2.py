8ddff9895e13c1ffdc6b39763388bda5d2324494
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-07-06 12:10:07 -0400

    545 Accepted Solution

---
+++ 126_Word_Ladder_2.py
@@ -0,0 +1,67 @@
+from collections import deque
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+
+        def dfs(curword):
+
+            res.append(curword)
+
+            if curword == endWord:
+                ans.append(res[:])
+                res.pop()
+                return
+
+            if curword in adjlist:
+                for neighbour in adjlist[curword]:
+                    if min_distance[neighbour] == min_distance[curword] + 1:
+                        dfs(neighbour)
+
+            res.pop()
+            return
+
+        wordList = set(wordList)
+        queue = deque([])
+        adjlist = {}
+        min_distance = {}
+        visited = set()
+        alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+        queue.append((beginWord, 0))
+        visited.add(beginWord)
+
+        while queue:
+
+            curword, distance = queue.popleft()
+
+            min_distance[curword] = distance
+
+            for index, char in enumerate(curword):
+                for alpha in alphabets:
+                    if alpha != char:
+                        newWord = curword[: index] + alpha + curword[index + 1:]
+
+                        if newWord in wordList:
+                            if newWord not in adjlist:
+                                adjlist[newWord] = set()
+                            adjlist[newWord].add(curword)
+
+                            if curword not in adjlist:
+                                adjlist[curword] = set()
+                            adjlist[curword].add(newWord)
+
+                            if newWord not in visited:
+                                visited.add(newWord)
+                                queue.append((newWord, distance + 1))
+
+        if endWord not in adjlist:
+            return []
+
+        # print(min_distance)
+        # print(adjlist)
+
+        ans = []
+        res = []
+        dfs(beginWord)
+        return ans
