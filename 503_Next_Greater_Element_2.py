cac8c643caeaafe0c240304a36c1392460ad0f9f
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-07-07 10:46:37 -0400

    503 Accepted Stack Solution with Commented Failing on Last Test Case O(n^2) Solution

--- 503_Next_Greater_Element_2.py
+++ 503_Next_Greater_Element_2.py
@@ -1,17 +1,40 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
 
+        #         n = len(nums)
+        #         ans = [-1 for _ in range(n)]
+
+        #         for i in range(len(nums)):
+        #             current_num = nums[i]
+
+        #             for j in range(len(nums)):
+        #                 next_num = nums[(i+j)%n]
+
+        #                 if next_num > current_num:
+        #                     ans[i] = next_num
+        #                     break
+
+        #         return ans
+
         n = len(nums)
-        ans = [-1 for _ in range(n)]
+        res = [-1 for _ in range(n)]
+        stack = []
 
-        for i in range(len(nums)):
-            current_num = nums[i]
+        for i in range(2 * n):
 
-            for j in range(len(nums)):
-                next_num = nums[(i + j) % n]
+            current_index = i % n
+            current_element = nums[current_index]
 
-                if next_num > current_num:
-                    ans[i] = next_num
-                    break
+            if not stack or current_element < nums[stack[-1]]:
+                stack.append(current_index)
 
-        return ans
+            else:
+                while stack and current_element > nums[stack[-1]]:
+                    top = stack.pop()
+
+                    if res[top] == -1:
+                        res[top] = current_element
+
+                stack.append(current_index)
+
+        return res
