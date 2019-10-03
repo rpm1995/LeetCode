cafd5bbf2616d944ef99eaecf7e0ec3c8425fd1b
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-10-02 23:01:30 -0400

    225 Accepted Solution

---
+++ 225_Implement_Stack_Using_Queues.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = deque([])
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+        length = len(self.queue)
+
+        for _ in range(length - 1):  # Brings x to the head of the queue
+
+            temp = self.queue.popleft()
+            self.queue.append(temp)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not self.queue
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
