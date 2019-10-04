78548c5f0ff6b00ae5d3f36c1c04fadc1f647c5c
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-10-04 10:47:19 -0400

    935 TLE Cached Brute Force

--- 935_Knight_Dialer.py
+++ 935_Knight_Dialer.py
@@ -1,6 +1,33 @@
 class Solution:
     def knightDialer(self, N: int) -> int:
 
+        #         mappings = {0: [4, 6],
+        #                    1: [6, 8],
+        #                    2: [9, 7],
+        #                    3: [4, 8],
+        #                    4: [3, 9, 0],
+        #                    5: [],
+        #                    6: [1, 7, 0],
+        #                    7: [2, 6],
+        #                    8: [1, 3],
+        #                    9: [2, 4]}
+
+        #         self.ans = 0
+
+        #         def helper(hops_left, current_digit, current_number):
+
+        #             if hops_left == 0:
+        #                 self.ans += 1
+
+        #             else:
+        #                 for mapping in mappings[current_digit]:
+        #                     helper(hops_left-1, mapping, current_number*10 + mapping)
+
+        #         for first_digit in range(0, 10):
+        #             helper(N-1, first_digit, first_digit)
+
+        #         return self.ans
+
         mappings = {0: [4, 6],
                     1: [6, 8],
                     2: [9, 7],
@@ -13,19 +40,32 @@ class Solution:
                     9: [2, 4]}
 
         self.ans = 0
-        numbers_seen = set()
+        cache = {}
+        mod = 10 ** 9 + 7
 
-        def helper(hops_left, current_digit, current_number):
+        def helper(hops_left, current_digit, current_number, current_length):
+
+            if (current_digit, hops_left) in cache:
+                return cache[(current_digit, hops_left)]
 
             if hops_left == 0:
-                self.ans += 1
-                numbers_seen.add(current_number)
+                return 1
 
             else:
+                length = 0
+
                 for mapping in mappings[current_digit]:
-                    helper(hops_left - 1, mapping, current_number * 10 + mapping)
+                    length += helper(hops_left - 1, mapping, current_number * 10 + mapping, current_length + 1)
+
+                cache[(current_digit, hops_left)] = length
+
+            # self.ans += cache[(current_digit, hops_left)]
+            return cache[(current_digit, hops_left)]
+
+        if N == 1:
+            return 10
 
         for first_digit in range(0, 10):
-            helper(N - 1, first_digit, first_digit)
+            self.ans += helper(N - 1, first_digit, first_digit, 1)
 
-        return self.ans
+        return self.ans % mod
