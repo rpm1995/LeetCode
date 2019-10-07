ead0599c01eb1346f5e452dd6d7b7a03b2bdf6cb
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2019-10-06 21:42:23 -0400

    974 Accepted O(n) Solution

--- 974_Subarray_Sums_Divisible_By_K.py
+++ 974_Subarray_Sums_Divisible_By_K.py
@@ -1,18 +1,36 @@
 class Solution:
     def subarraysDivByK(self, A: List[int], K: int) -> int:
 
-        prefix_sum = [0 for _ in range(len(A) + 1)]
+        #         prefix_sum = [0 for _ in range(len(A) + 1)]
+        #         ans = 0
+
+        #         for index, element in enumerate(A):
+        #             prefix_sum[index + 1] = prefix_sum[index] + element
+
+        #         for first_index, first_element in enumerate(prefix_sum):
+        #             for second_index, second_element in enumerate(prefix_sum[first_index+1:]):
+
+        #                 sub_array_sum = second_element - first_element
+
+        #                 if sub_array_sum % K == 0:
+        #                     ans += 1
+
+        #         return ans
+
+        prefix_mods = {0: 1}
         ans = 0
+        running_sum = 0
 
         for index, element in enumerate(A):
-            prefix_sum[index + 1] = prefix_sum[index] + element
 
-        for first_index, first_element in enumerate(prefix_sum):
-            for second_index, second_element in enumerate(prefix_sum[first_index + 1:]):
+            running_sum += element
+            remainder = running_sum % K
 
-                sub_array_sum = second_element - first_element
+            if remainder in prefix_mods:
+                ans += prefix_mods[remainder]
 
-                if sub_array_sum % K == 0:
-                    ans += 1
+            if remainder not in prefix_mods:
+                prefix_mods[remainder] = 0
+            prefix_mods[remainder] += 1
 
         return ans
