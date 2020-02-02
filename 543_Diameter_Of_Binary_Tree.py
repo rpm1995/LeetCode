63da682c55aa1500daf50ac5e7a0b465561c1d48
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-02-01 21:42:14 -0700

    Added Solution with no Global Variable

--- 543_Diameter_Of_Binary_Tree.py
+++ 543_Diameter_Of_Binary_Tree.py
@@ -21,3 +21,37 @@ class Solution:
         self.ans = 0
         helper(root)
         return self.ans
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+
+        if not root:
+            return 0
+
+        # self.ans = -float('inf')
+
+        def helper(node, ans):
+
+            if not node:
+                return 0
+
+            L = helper(node.left, ans)
+            R = helper(node.right, ans)
+
+            # self.ans = max(self.ans, L + R)
+            ans[0] = max(ans[0], L + R)
+
+            return max(L, R) + 1
+
+        ans = [-float('inf')]
+        helper(root, ans)
+        return ans[0]
+
