ae7b1d85923a6c7b8b6b96c75a2d6dd503109ebc
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-02-01 17:02:50 -0700

    Added Recursive Solution

--- 206_Reverse_Linked_List.py
+++ 206_Reverse_Linked_List.py
@@ -24,3 +24,27 @@ class Solution:
                 nex = nex.next
 
         return prev
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def reverse(node):
+            if not node or not node.next:
+                self.head = node
+                return node
+
+            reverse(node.next)
+            node.next.next = node
+            node.next = None
+
+            return node
+
+        self.head = ListNode(-1)
+        reverse(head)
+        return self.head
+
