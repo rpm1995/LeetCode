cde05ab6802708a9f1b9e0c9dd45875e8abbcae9
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-02-09 22:06:54 -0600

    Accepted Union Find Solution

--- 547_Friend_Circles.py
+++ 547_Friend_Circles.py
@@ -40,3 +40,59 @@ class Solution:
 
 
         return circles
+
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+
+        class unionFind:
+
+            def __init__(self, n):
+
+                self.count = n
+                self.parent = [i for i in range(n)]
+                self.rank = [0 for _ in range(n)]
+
+            def find(self, x):
+
+                if self.parent[x] == x:
+                    return x
+
+                self.parent[x] = self.find(self.parent[x])
+                return self.parent[x]
+
+            def union(self, x, y):
+
+                x_node = self.find(self.parent[x])
+                y_node = self.find(self.parent[y])
+
+                if x_node != y_node:
+
+                    if self.rank[x_node] < self.rank[y_node]:
+                        self.parent[x_node] = y_node
+
+                    else:
+                        self.parent[y_node] = x_node
+
+                        if self.rank[x_node] == self.rank[y_node]:
+                            self.rank[x_node] += 1
+
+                    self.count -= 1
+                return
+
+            def get_count(self):
+
+                return self.count
+
+        n = len(M)
+        union_tree = unionFind(n)
+
+        for friend1 in range(n):
+            for friend2 in range(friend1 + 1, n):
+
+                if friend1 != friend2 and M[friend1][friend2] == 1:
+                    union_tree.union(friend1, friend2)
+
+        return union_tree.get_count()
+
+
