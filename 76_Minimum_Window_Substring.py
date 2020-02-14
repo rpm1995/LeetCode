920c5c87688e663efec263db26492c4203e27810
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-02-13 20:14:15 -0600

    Accepted Solution

--- 76_Minimum_Window_Substring.py
+++ 76_Minimum_Window_Substring.py
@@ -3,44 +3,42 @@ class Solution:
 
         s_dict = {}
         t_dict = {}
-        s_unique = 0
-        t_unique = 0
+        similarity = 0
         left = 0
         right = 0
         ans = ""
+        min_length = float('inf')
 
         for char in t:
             if char not in t_dict:
                 t_dict[char] = 0
-                t_unique += 1
             t_dict[char] += 1
 
         while right < len(s):
+            char = s[right]
 
-            new_char = s[right]
+            if char in t_dict:
+                if char not in s_dict:
+                    s_dict[char] = 0
+                s_dict[char] += 1
 
-            if new_char in t_dict:
+                if s_dict[char] == t_dict[char]:
+                    similarity += 1
 
-                if new_char not in s_dict:
-                    s_dict[new_char] = 0
-                s_dict[new_char] += 1
+                while similarity == len(t_dict):
+                    if right - left + 1 < min_length:
+                        min_length = right - left + 1
+                        ans = s[left: right + 1]
 
-                if s_dict[new_char] == t_dict[new_char]:
-                    s_unique += 1
+                    left_char = s[left]
 
-                while left <= right and s_unique >= t_unique:
-                    ans = s[left:right + 1] if len(ans) > right - left + 1 or ans == "" else ans
+                    if left_char in t_dict:
+                        s_dict[left_char] -= 1
 
-                    old_char = s[left]
-
-                    if old_char in s_dict:
-                        s_dict[old_char] -= 1
-
-                        if s_dict[old_char] < t_dict[old_char]:
-                            s_unique -= 1
+                        if s_dict[left_char] < t_dict[left_char]:
+                            similarity -= 1
 
                     left += 1
-
             right += 1
 
         return ans
