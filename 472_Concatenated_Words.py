b891fa367cfe810f2f9cfbc236814089b72a41e9
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-02-15 20:46:34 -0600

    Easier to read Solution

--- 472_Concatenated_Words.py
+++ 472_Concatenated_Words.py
@@ -1,31 +1,33 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
 
-        def helper(word):
-
-            if not word:
-                return True
-
-            for i in range(1, len(word) + 1):
-                prefix = word[:i]
-                suffix = word[i:]
-
-                if prefix in wordset:
-                    if suffix in wordset:
-                        return True
-                    if helper(suffix):
-                        return True
-
-            return False
-
-        wordset = set(words)
+        words_set = set(words)
         ans = []
 
+        def is_concatenated(word, start, words_set, cache):
+            if start >= len(word):
+                return True
+
+            if cache[start] is not None:
+                return cache[start]
+
+            for end in range(start, len(word) + 1):
+                if word[start: end + 1] in words_set:
+                    if is_concatenated(word, end + 1, words_set, cache):
+                        cache[start] = True
+                        return cache[start]
+
+            cache[start] = False
+            return cache[start]
+
         for word in words:
-            if word:  # Special Test Case
-                wordset.remove(word)
-                if helper(word) is True:
+            if word:  # Silly test case
+                words_set.remove(
+                    word)  # We do this because we do not want the smaller words itself to be part of the answer
+
+                if is_concatenated(word, 0, words_set, [None for _ in range(len(word))]):
                     ans.append(word)
-                wordset.add(word)
+
+                words_set.add(word)
 
         return ans
