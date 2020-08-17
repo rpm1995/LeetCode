9b19c0d1cfa0e8be8329f22bf31299155a5d21cf
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-08-16 21:45:43 -0500

    Accepted Coloring Solution with Iterative DFS

---
+++ 785_Is_Graph_Bipartite.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+
+        color = [-1 for _ in range(len(graph))]
+
+        for node in range(len(graph)):
+
+            if color[node] == -1:
+                stack = [node]
+                color[node] = 0
+
+                while stack:
+                    cur_node = stack.pop()
+                    cur_color = color[cur_node]
+
+                    for neighbour in graph[cur_node]:
+
+                        if color[neighbour] == -1:
+                            color[neighbour] = cur_color ^ 1
+                            stack.append(neighbour)
+
+                        elif color[neighbour] == cur_color:
+                            return False
+
+                    cur_color ^= 1
+
+        return True
