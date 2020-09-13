78a69b34b9d1b193a31c895acbc9793dc3c1b5ac
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-09-12 19:19:41 -0500

    Accepted DP Solutions

--- 53_Maximum_Subarray.py
+++ 53_Maximum_Subarray.py
@@ -1,15 +1,20 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #         ans = -float('inf')
+        #         sum_ = -float('inf')
 
-        maxsum = nums[0]
-        sumtillhere = nums[0]
+        #         for index, element in enumerate(nums):
 
-        for i in range(1, len(nums)):
-            sumtillhere = max(sumtillhere + nums[i], nums[i])
-            maxsum = max(maxsum, sumtillhere)
+        #             sum_ = max(sum_ + element, element)
+        #             ans = max(ans, sum_)
 
-        return maxsum
+        #         return ans
+
+        n = len(nums)
+        dp = [0 for _ in range(n)]
+        dp[0] = nums[0]
+
+        for i in range(1, n):
+            dp[i] = max(nums[i] + dp[i - 1], nums[i])
+
+        return max(dp)
