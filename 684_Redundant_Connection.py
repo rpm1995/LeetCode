ac3344dce00da776c66dd94e63d002a82f0211fe
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-10-28 22:53:30 -0500

    Accepted Union Find Solution

--- 684_Redundant_Connection.py
+++ 684_Redundant_Connection.py
@@ -1,33 +1,59 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 
-        adj_list = {}
+        #         adj_list = {}
 
-        def dfs(cur, target):
+        #         def dfs(cur, target):
 
-            if cur == target:
-                return True
+        #             if cur == target:
+        #                 return True
 
-            if cur not in visited:
-                visited.add(cur)
+        #             if cur not in visited:
+        #                 visited.add(cur)
 
-            for neighbour in adj_list[cur]:
-                if neighbour not in visited and dfs(neighbour, target) is True:
-                    return True
+        #             for neighbour in adj_list[cur]:
+        #                 if neighbour not in visited and dfs(neighbour, target) is True:
+        #                     return True
 
-            return False
+        #             return False
+
+        #         for node1, node2 in edges:
+
+        #             visited = set()
+
+        #             if node1 in adj_list and node2 in adj_list and dfs(node1, node2):
+        #                 return node1, node2
+
+        #             if node1 not in adj_list:
+        #                 adj_list[node1] = set()
+        #             adj_list[node1].add(node2)
+
+        #             if node2 not in adj_list:
+        #                 adj_list[node2] = set()
+        #             adj_list[node2].add(node1)
+
+        def find(node):
+
+            if parent[node] == 0:
+                return node
+
+            parent[node] = find(parent[node])
+            return parent[node]
+
+        def union(node1, node2):
+
+            root1 = find(node1)
+            root2 = find(node2)
+
+            if root1 == root2:
+                return False
+
+            parent[root1] = root2
+            return True
+
+        parent = [0 for _ in range(len(edges))]
 
         for node1, node2 in edges:
 
-            visited = set()
-
-            if node1 in adj_list and node2 in adj_list and dfs(node1, node2):
+            if not union(node1 - 1, node2 - 1):
                 return node1, node2
-
-            if node1 not in adj_list:
-                adj_list[node1] = set()
-            adj_list[node1].add(node2)
-
-            if node2 not in adj_list:
-                adj_list[node2] = set()
-            adj_list[node2].add(node1)
--- 950_Reveal_Cards_In_Increasing_Order.py
+++ 950_Reveal_Cards_In_Increasing_Order.py
@@ -1,59 +1,23 @@
+from collections import deque
+
+
 class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
 
-        #         adj_list = {}
+        n = len(deck)
+        index = [i for i in range(n)]
+        index = deque(index)
+        deck.sort()
+        ans = [-1 for _ in range(n)]
+        current = 0
 
-        #         def dfs(cur, target):
+        while index:
 
-        #             if cur == target:
-        #                 return True
+            popped = index.popleft()
+            ans[popped] = deck[current]
 
-        #             if cur not in visited:
-        #                 visited.add(cur)
+            if index:
+                index.append(index.popleft())
+                current += 1
 
-        #             for neighbour in adj_list[cur]:
-        #                 if neighbour not in visited and dfs(neighbour, target) is True:
-        #                     return True
-
-        #             return False
-
-        #         for node1, node2 in edges:
-
-        #             visited = set()
-
-        #             if node1 in adj_list and node2 in adj_list and dfs(node1, node2):
-        #                 return node1, node2
-
-        #             if node1 not in adj_list:
-        #                 adj_list[node1] = set()
-        #             adj_list[node1].add(node2)
-
-        #             if node2 not in adj_list:
-        #                 adj_list[node2] = set()
-        #             adj_list[node2].add(node1)
-
-        def find(node):
-
-            if parent[node] == 0:
-                return node
-
-            parent[node] = find(parent[node])
-            return parent[node]
-
-        def union(node1, node2):
-
-            root1 = find(node1)
-            root2 = find(node2)
-
-            if root1 == root2:
-                return False
-
-            parent[root1] = root2
-            return True
-
-        parent = [0 for _ in range(len(edges))]
-
-        for node1, node2 in edges:
-
-            if not union(node1 - 1, node2 - 1):
-                return node1, node2
+        return ans
