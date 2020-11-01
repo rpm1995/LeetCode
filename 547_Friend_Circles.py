0caadf2f0ef76c934bfe248c95f973f498433ea4
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-11-01 15:34:07 -0600

    Accepted Union Find Solution

--- 547_Friend_Circles.py
+++ 547_Friend_Circles.py
@@ -96,3 +96,72 @@ class Solution:
         return union_tree.get_count()
 
 
+######################## Commented Approach works!
+
+class UnionFind:
+
+    def __init__(self, size):
+
+        self.parents = [i for i in range(size)]
+        self.components = size
+        self.component_size = {i: 1 for i in range(size)}
+
+    def find(self, node):
+
+        if self.parents[node] == node:
+            return node
+
+        self.parents[node] = self.find(self.parents[node])
+
+        return self.parents[node]
+
+    def getComponents(self):
+
+        return self.components
+
+    def union(self, node1, node2):
+
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+
+        if root1 == root2:
+            return
+
+        if self.component_size[root1] > self.component_size[root2]:
+            self.parents[root2] = root1
+            self.component_size[root2] += self.component_size[root1]
+
+        else:
+            self.parents[root1] = root2
+            self.component_size[root1] += self.component_size[root2]
+
+        self.components -= 1
+
+    # def cleanUp(self):
+    #
+    #     for kids in self.parents:
+    #         temp = self.find(kids)
+
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+
+        n = len(M)
+        disjoint_set = UnionFind(n)
+        # ans = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if M[i][j] == 1:
+                    disjoint_set.union(i, j)
+
+        # disjoint_set.cleanUp()
+        #
+        # for index, kids in enumerate(disjoint_set.parents):
+        #     if index == kids:
+        #         ans += 1
+        #
+        # return ans
+
+        return disjoint_set.components
+
