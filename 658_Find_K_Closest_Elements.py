159c5911399578e37c0bd389ff800222c96c0c4d
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-11-05 21:20:54 -0600

    Accepted O(log n) Solution

--- 658_Find_K_Closest_Elements.py
+++ 658_Find_K_Closest_Elements.py
@@ -1,15 +1,38 @@
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+#         # O(n) Solution
+#         left = 0
+#         right = len(arr) - 1
+#
+#         while right - left + 1 > k:
+#
+#             if abs(arr[left] - x) > abs(arr[right] - x):
+#                 left += 1
+#
+#             else:
+#                 right -= 1
+#
+#         return arr[left: right + 1]
+
+
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # O(n) Solution
+
+        # approach: use binary search to find the start which is closest to x
+
         left = 0
-        right = len(arr) - 1
+        right = len(arr) - k
 
-        while right - left + 1 > k:
+        while left < right:
+            mid = left + (right - left) // 2
 
-            if abs(arr[left] - x) > abs(arr[right] - x):
-                left += 1
+            # mid + k is closer to x, discard mid by assigning left = mid + 1
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
 
+            # mid is equal or closer to x than mid + k, remains mid as candidate
             else:
-                right -= 1
+                right = mid
 
-        return arr[left: right + 1]
+        # left == right, which makes both left and left + k have same diff with x
+        return arr[left: left + k]
