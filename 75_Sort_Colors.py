9c33cba4b0790db208e8b5d4cb5186f105934196
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2020-11-26 09:47:55 -0600

    Accepted Solution

--- 75_Sort_Colors.py
+++ 75_Sort_Colors.py
@@ -4,33 +4,50 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
+        #         zeros = 0
+        #         ones = 0
+        #         twos = len(nums) - 1
+
+        #         while ones <= twos:
+
+        #             element = nums[ones]
+
+        #             if element == 1:
+        #                 ones += 1
+
+        #             elif element == 2:
+        #                 temp = nums[ones]
+        #                 nums[ones] = nums[twos]
+        #                 nums[twos] = temp
+        #                 # nums[ones], nums[twos] = nums[twos], nums[ones]
+        #                 twos -= 1
+        #                 # ones += 1
+
+        #             elif element == 0:
+        #                 temp = nums[ones]
+        #                 nums[ones] = nums[zeros]
+        #                 nums[zeros] = temp
+        #                 # nums[ones], nums[zeros] = nums[zeros], nums[ones]
+        #                 zeros += 1
+        #                 ones += 1
+
         zeros = 0
-        ones = 0
         twos = len(nums) - 1
+        curr = 0
 
-        while ones <= twos:
+        while curr <= twos:
 
-            element = nums[ones]
+            num = nums[curr]
 
-            if element == 1:
-                ones += 1
-
-            elif element == 2:
-                temp = nums[ones]
-                nums[ones] = nums[twos]
-                nums[twos] = temp
-                twos -= 1
-                # ones += 1
-
-            elif element == 0:
-                temp = nums[ones]
-                nums[ones] = nums[zeros]
-                nums[zeros] = temp
+            if nums[curr] == 0:
+                nums[zeros], nums[curr] = nums[curr], nums[zeros]
                 zeros += 1
-                ones += 1
+                curr += 1
 
-            # print("zeros: " + str(zeros))
-            # print("ones: " + str(ones))
-            # print("element: " + str(element))
-            # print("twos: " + str(twos))
-            # print(nums)
+            elif nums[curr] == 2:
+                nums[twos], nums[curr] = nums[curr], nums[twos]
+                twos -= 1
+                # curr += 1
+
+            else:
+                curr += 1
