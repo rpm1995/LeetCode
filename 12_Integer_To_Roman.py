5ca7b697d03037eda7719164c2bd4f8d61ef2d2e
rpm1995 <31997276+rpm1995@users.noreply.github.com>
2021-07-09 21:34:18 -0500

    Accepted Solution

--- 12_Integer_To_Roman.py
+++ 12_Integer_To_Roman.py
@@ -1,3 +1,22 @@
+# class Solution:
+#     def intToRoman(self, num: int) -> str:
+#
+#         romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+#         values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+#         ans = ""
+#
+#         pointer = 0
+#
+#         while num > 0:
+#
+#             if num - values[pointer] >= 0:
+#                 ans += romans[pointer]
+#                 num -= values[pointer]
+#             else:
+#                 pointer += 1
+#
+#         return ans
+
 class Solution:
     def intToRoman(self, num: int) -> str:
 
@@ -9,10 +28,21 @@ class Solution:
 
         while num > 0:
 
-            if num - values[pointer] >= 0:
-                ans += romans[pointer]
-                num -= values[pointer]
+            # if num - values[pointer] >= 0:
+            #     ans += romans[pointer]
+            #     num -= values[pointer]
+            # else:
+            #     pointer += 1
+
+            times = num // values[pointer]
+
+            if times > 0:
+
+                ans = ans + (times * romans[pointer])
+                num %= values[pointer]
+
             else:
                 pointer += 1
 
         return ans
+
