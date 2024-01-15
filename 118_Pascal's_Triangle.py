918f3fef58e1d7dd7d43a7362490de4cf629c69c
Rohit Mathew <rohit@Rohits-MBP.attlocal.net>
2024-01-14 20:58:16 -0600

    Code for Pascal's Triangle

--- 118_Pascal's_Triangle.py
+++ 118_Pascal's_Triangle.py
@@ -1,14 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1: return [[1]]
+        
+        pascalsTriangle = [[1], [1,1]]
 
-        ans = [[1]]
+        for currentRow in range(2, numRows):
+            newRow = [1]
 
-        for row in range(2, numRows + 1):
-            res = [1 for _ in range(row)]
-
-            for col in range(1, row - 1):
-                res[col] = ans[-1][col - 1] + ans[-1][col]
-
-            ans.append(res)
-
-        return ans
+            for currentCol in range(1, currentRow):
+                newRow.append(pascalsTriangle[currentRow-1][currentCol-1] + pascalsTriangle[currentRow-1][currentCol])
+            
+            newRow.append(1)
+            pascalsTriangle.append(newRow)
+        
+        return pascalsTriangle
